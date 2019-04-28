8d774e55f223bb466c379b96759e369e856554ec
fbiernat <filipbiernat@gmail.com>
2019-04-28 14:40:33 +0200

    Implement registration form

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -4,6 +4,10 @@
       <img src="./assets/logo.svg" alt="Enroller" class="logo">
       System do zapisów na zajęcia
     </h1>
+    <div>
+      <button class="button" v-bind:class="{ 'button-outline': !isLogin}" @click="showLoginForm()">Loguję się</button>
+      <button class="button" v-bind:class="{ 'button-outline': isLogin}" @click="showRegisterForm()">Rejestruję się</button>
+    </div>
     <div v-if="authenticatedUsername">
       <h2>Witaj {{ authenticatedUsername }}!
         <a @click="logout()" class="float-right  button-outline button">Wyloguj</a>
@@ -11,7 +15,8 @@
       <meetings-page :username="authenticatedUsername"></meetings-page>
     </div>
     <div v-else>
-      <login-form @login="login($event)"></login-form>
+      <login-form v-if="isLogin" @login="login($event)"></login-form>
+      <login-form v-else @login="register($event)" button-label="Zarejestruj się"></login-form>
     </div>
   </div>
 </template>
@@ -20,11 +25,12 @@
     import "milligram";
     import LoginForm from "./LoginForm";
     import MeetingsPage from "./meetings/MeetingsPage";
-
+ 
     export default {
         components: {LoginForm, MeetingsPage},
         data() {
             return {
+                isLogin: true,
                 authenticatedUsername: ""
             };
         },
@@ -34,6 +40,25 @@
             },
             logout() {
                 this.authenticatedUsername = '';
+            },
+            register(user) {
+              if(user.login != '' && user.password != '')
+                // rejestruj uzytkownika
+                this.$http.post('participants', user)
+                  .then(response => {
+                    // udało się
+                    alert('Pomyślnie utworzono użytkownika!');
+                  })
+                  .catch(response => {
+                    // nie udało się
+                    alert('Ten użytkownik już istnieje');
+                  })
+            },
+            showLoginForm() {
+              this.isLogin = true;
+            },
+            showRegisterForm() {
+              this.isLogin = false;
             }
         }
     };
