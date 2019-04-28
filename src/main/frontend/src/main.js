fd1e0d92587a8cd9d365aec5962c3b92805360e0
fbiernat <filipbiernat@gmail.com>
2019-04-28 21:13:22 +0200

    Add vue-notification dependency

--- src/main/frontend/src/main.js
+++ src/main/frontend/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
 import VueResource from "vue-resource";
+import Notifications from 'vue-notification';
 
 Vue.use(VueResource);
+Vue.use(Notifications);
 Vue.http.options.root = '/api';
 
 new Vue({
