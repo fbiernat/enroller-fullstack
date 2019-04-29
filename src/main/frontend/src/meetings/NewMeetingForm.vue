16b67835beba0148ea594c40297f342019027e12
fbiernat <filipbiernat@gmail.com>
2019-04-29 21:08:21 +0200

    Add datepicker input to set meeting's date

--- src/main/frontend/src/meetings/NewMeetingForm.vue
+++ src/main/frontend/src/meetings/NewMeetingForm.vue
@@ -6,6 +6,8 @@
       <input type="text" v-model="newMeeting.name">
       <label>Opis</label>
       <textarea v-model="newMeeting.description"></textarea>
+      <label>Data</label>
+      <input type="date" v-model="newMeeting.date" id="date-picker">
       <button>Dodaj</button>
       <span class="error" v-if="error">Spotkanie musi mieć nazwę!</span>
     </form>
@@ -26,6 +28,7 @@
             addNewMeeting() {
                 this.error = false;
                 if (this.newMeeting.name) {
+                  console.log(this.newMeeting.date);
                     this.$emit('added', this.newMeeting);
                     this.newMeeting = {participants: []};
                     this.adding = false;
@@ -35,10 +38,32 @@
             }
         }
     }
+
+    function getTodaysDate() {
+      let date = new Date();
+      return date.getFullYear() + '-' + (date.getMonth() < 10 ? '0' : '') + date.getMonth() + '-' + (date.getDay() < 10 ? '0' : '') + date.getDay();
+    }
+
+    // Doesn't work :/
+    document.addEventListener('DOMElementsLoaded', function() {
+      document.getElementById('date-picker').value = getTodaysDate();
+    });
 </script>
 
 <style scoped>
   .error {
     color: #F00;
+  }
+
+  #date-picker {
+    display: block;
+    background-color: transparent;
+    border: 0.1rem solid #d1d1d1;
+    border-radius: .4rem;
+    box-shadow: none;
+    box-sizing: inherit;
+    height: 3.8rem;
+    padding: .6rem 1.0rem;
+    width: 100%;
   }
 </style>
