8811e888ed6fd843cd396933209ee6cd4e800a42
fbiernat <filipbiernat@gmail.com>
2019-04-29 21:11:21 +0200

    Add MeetingRestControllerTest.java

---
+++ src/test/java/com/company/enroller/controllers/MeetingRestControllerTest.java
@@ -0,0 +1,39 @@
+package com.company.enroller.controllers;
+
+import static java.util.Collections.singletonList;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.company.enroller.persistence.MeetingService;
+import com.company.enroller.persistence.ParticipantService;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(MeetingRestController.class)
+public class MeetingRestControllerTest {
+	@Autowired
+	private MockMvc mvc;
+
+	@MockBean
+	private MeetingService meetingService;
+
+	@MockBean
+	private ParticipantService participantService;
+
+	@Test
+	public void testGetAllMeeting() {
+		
+	}
+	
+}
