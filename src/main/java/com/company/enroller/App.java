5bc88e01bc63062994a5ffc28d11e8782c7115a7
fbiernat <filipbiernat@gmail.com>
2019-04-30 17:32:19 +0200

    Change indents

--- src/main/java/com/company/enroller/App.java
+++ src/main/java/com/company/enroller/App.java
@@ -9,11 +9,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @SpringBootApplication
 public class App {
 
-	public static void main(String[] args) { SpringApplication.run(App.class, args); }
-	
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+    public static void main(String[] args) { SpringApplication.run(App.class, args); }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
