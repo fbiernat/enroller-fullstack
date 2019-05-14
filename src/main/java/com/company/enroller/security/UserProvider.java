4e965df08052bc0c1ee557b049cb2d9fb828f0ee
fbiernat <filipbiernat@gmail.com>
2019-05-14 22:44:36 +0200

    Create UserProvider class for getting currently logged in user login

--- src/main/java/com/company/enroller/security/UserProvider.java
+++ src/main/java/com/company/enroller/security/UserProvider.java
@@ -1,4 +1,19 @@
 package com.company.enroller.security;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
 public class UserProvider {
+
+    public static String getUsername() {
+        String username;
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails) {
+            username = ((UserDetails)principal).getUsername();
+        } else {
+            username = principal.toString();
+        }
+        return username;
+    }
+
 }
